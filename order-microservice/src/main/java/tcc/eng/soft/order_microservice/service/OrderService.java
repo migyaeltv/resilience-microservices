27236303ece33316b2539c105f7c4609ac6cf788
migyaeltv <migyael.t.v@gmail.com>
2025-02-13 18:04:16 -0300

    feature retry

--- order-microservice/src/main/java/tcc/eng/soft/order_microservice/service/OrderService.java
+++ order-microservice/src/main/java/tcc/eng/soft/order_microservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package tcc.eng.soft.order_microservice.service;
 import feign.FeignException;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.stereotype.Service;
 import tcc.eng.soft.order_microservice.domain.Order;
 import tcc.eng.soft.order_microservice.dto.OrderRequestDTO;
@@ -20,7 +21,7 @@ public class OrderService {
         this.orderRepository = orderRepository;
         this.paymentClient = paymentClient;
     }
-
+    @Retry(name = "orderServiceRetry", fallbackMethod = "fallbackOrder")
     public OrderResponseDTO createOrder(OrderRequestDTO orderRequest) {
         Order order = new Order();
         order.setCustomerId(orderRequest.getCustomerId());
@@ -32,18 +33,31 @@ public class OrderService {
             PaymentRequestDTO paymentRequest = new PaymentRequestDTO(order.getId(), order.getAmount());
             PaymentResponseDTO paymentResponse = paymentClient.processPayment(paymentRequest);
 
-            // Actualizar el estado del pedido según la respuesta del pago
+
             String paymentStatus = paymentResponse.getStatus().equals("PAID") ? "PAID" : "FAILED";
             order.setStatus(paymentStatus);
         }
 
         catch (FeignException e) {
-            System.err.println("Error en PaymentService: " + e.getMessage());
-            order.setStatus("FAILED"); // Marcar la orden como fallida si PaymentService falla
+            System.err.println("Erro em PaymentService: " + e.getMessage());
+            order.setStatus("FAILED");
         }
 
         orderRepository.save(order);
 
         return new OrderResponseDTO(order.getId(), order.getCustomerId(), order.getAmount(), order.getStatus());
     }
+
+    public OrderResponseDTO fallbackOrder(OrderRequestDTO orderRequest, Exception e) {
+        System.err.println("PaymentService não disponivel: " + e.getMessage());
+
+        Order order = new Order();
+        order.setCustomerId(orderRequest.getCustomerId());
+        order.setAmount(orderRequest.getAmount());
+        order.setStatus("PENDING_PAYMENT");
+        orderRepository.save(order);
+
+        return new OrderResponseDTO(order.getId(), order.getCustomerId(), order.getAmount(), "PENDING_PAYMENT");
+    }
+
 }
