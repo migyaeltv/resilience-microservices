f65bcea36e13e19d2812b67eb5e6f31b981d7586
migyaeltv <migyael.t.v@gmail.com>
2025-02-28 22:36:53 -0300

    circuit break

--- order-microservice/src/main/java/tcc/eng/soft/order_microservice/service/OrderService.java
+++ order-microservice/src/main/java/tcc/eng/soft/order_microservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package tcc.eng.soft.order_microservice.service;
 import feign.FeignException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.stereotype.Service;
 import tcc.eng.soft.order_microservice.domain.Order;
@@ -21,7 +22,8 @@ public class OrderService {
         this.orderRepository = orderRepository;
         this.paymentClient = paymentClient;
     }
-    @Retry(name = "orderServiceRetry", fallbackMethod = "fallbackOrder")
+    
+    @CircuitBreaker(name = "paymentCircuitBreaker", fallbackMethod = "fallbackPayment")
     public OrderResponseDTO createOrder(OrderRequestDTO orderRequest) {
         Order order = new Order();
         order.setCustomerId(orderRequest.getCustomerId());
@@ -39,9 +41,9 @@ public class OrderService {
         }
 
         catch (FeignException e) {
-            if (e.status() >= 500 && e.status() < 600) {  // Somente reintentar se for erro 5xx
-                System.err.println("Erro 5xx detectado, aplicando retry: " + e.getMessage());
-                throw e;
+            if (e.status() >= 500 && e.status() < 600) {
+                System.err.println("Erro 5xx no serviço de pagamento, ativando Circuit Breaker.");
+                throw e;  // O Circuit Breaker vai gerenciar a resposta
             }
             order.setStatus("FAILED"); // Se não for 5xx, marca a ordem como falha
         }
@@ -50,17 +52,17 @@ public class OrderService {
 
         return new OrderResponseDTO(order.getId(), order.getCustomerId(), order.getAmount(), order.getStatus());
     }
-
-    public OrderResponseDTO fallbackOrder(OrderRequestDTO orderRequest, Exception e) {
-        System.err.println("PaymentService não disponivel: " + e.getMessage());
+    
+    public OrderResponseDTO fallbackPayment(OrderRequestDTO orderRequest, Exception e) {
+        System.err.println("Pagamento indisponível! Circuit Breaker ativado. " + e.getMessage());
 
         Order order = new Order();
         order.setCustomerId(orderRequest.getCustomerId());
         order.setAmount(orderRequest.getAmount());
-        order.setStatus("PENDING_PAYMENT");
+        order.setStatus("PENDING");
         orderRepository.save(order);
 
-        return new OrderResponseDTO(order.getId(), order.getCustomerId(), order.getAmount(), "PENDING_PAYMENT");
+        return new OrderResponseDTO(order.getId(), order.getCustomerId(), order.getAmount(), "PENDING");
     }
 
 }
